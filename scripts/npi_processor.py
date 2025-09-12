67610eda92bba983dba7cf085a990bf949eb2724
pbrennan2001 <pbrennan2001@gmail.com>
2025-09-12 19:12:25 -0400

    changed gitignore to exclude outputs, attempt at npi data processor, updated csv output function to work with polars

--- scripts/npi_processor.py
+++ scripts/npi_processor.py
@@ -1,2 +1,60 @@
+from os import rename
 import polars as pl
+import pandas as pd
+import time
+from utils.common_functions import save_to_formats
 
+# Load NPI data from a CSV file
+def load_npi_data(file_path):
+    start_time_polars = time.time()
+    npi_data = pl.read_csv(file_path)
+    end_time_polars = time.time()
+    print(f"Polars read_csv time: {end_time_polars - start_time_polars} seconds")
+    return npi_data
+
+# Run the function to load NPI data
+npi_data = load_npi_data('input/npidata_input.csv')
+
+# Clean the dataframe
+def clean_npi_data(npi_clean):
+    # Drop rows with null in the NPI column
+    npi_clean = npi_clean.drop_nulls(subset=["NPI"])
+
+    # Remove duplicates based on the NPI column (kept crashing)
+    # npi_clean = npi_clean.unique(subset=["NPI"])
+
+    return npi_clean
+
+npi_clean = clean_npi_data(npi_data)
+
+npi_clean
+
+# Process NPI data (select specific columns)
+def process_npi_data(npi_process):
+    
+    # Concatenate last, first, middle name into a single column
+    npi_process = npi_process.with_columns(
+        (pl.concat_str([npi_process["Provider First Name"], npi_process["Provider Last Name (Legal Name)"]], separator=" ")).alias("Provider Full Name")
+    )
+
+    npi_process = npi_process[
+        'NPI', 'Provider Full Name'
+    ]
+
+    npi_process = npi_process.with_columns(
+        pl.lit('2025-09-12').alias('last_updated')
+    )
+
+    npi_process = npi_process.rename({
+    'NPI': 'code',
+    'Provider Full Name': 'description'
+})
+
+    return npi_process
+
+npi_process = process_npi_data(npi_clean)
+
+npi_process
+
+# Save the processed data to CSV
+save_to_formats(npi_process, 'output/npi_processed')
--- utils/common_functions.py
+++ utils/common_functions.py
@@ -1,4 +1,9 @@
-
 # Resuasable function to save dataframe in csv format without index
 def save_to_formats(df, base_filename):
-    df.to_csv(f'{base_filename}.csv', index=False)
+    # Save as CSV using the correct method for pandas or polars
+    if hasattr(df, "to_csv"):
+        df.to_csv(f"{base_filename}.csv", index=False)
+    elif hasattr(df, "write_csv"):
+        df.write_csv(f"{base_filename}.csv")
+    else:
+        raise TypeError("Unsupported DataFrame type for saving to CSV.")
